a2509b40249950c5196f6f93236dcb99accad9ba
anvorguesa <srliposa@gmail.com>
2021-05-08 09:31:35 -0500

    ModelContactoReserva

---
+++ Models/ContactoReserva.cs
@@ -0,0 +1,10 @@
+namespace Labo.Models
+{
+    public class ContactoReserva
+    {
+        public string email { get; set; }
+        public string direc1{ get; set; }
+        public string direc2{ get; set; }
+        public string ciudad { get; set; }
+    }
+}
